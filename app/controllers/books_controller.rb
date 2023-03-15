4c766787734dfc1de0af79c07924e7fb72f5ac59
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-15 11:41:35 -0600

    Creating books.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new book_params
+
+    if @book.save
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :price)
+  end
 end
