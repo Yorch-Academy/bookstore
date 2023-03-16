cc2a3647a58da15d46e8534a4fd2b4a942f8ef7a
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-16 16:28:28 -0600

    Set total

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -3,5 +3,14 @@ class Sell < ApplicationRecord
   validates :quantity, presence: true
   validates :day, presence: true
 
+  after_create :set_total
+
   DAYS = %w[MONDAY TUESDAY WEDNESDAY THURSDAY FRIDAY SATURDAY SUNDAY].freeze
+
+  private
+
+  def set_total
+    revenue = quantity * book.price
+    update(total: revenue)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-book = Book.create(name: 'Harry Potter', price: 24.99)
+book = Book.create(name: 'Harry Potter', price: 24.49)
+book2 = Book.create(name: 'The Outliers', price: 19.99)
+book3 = Book.create(name: 'Sherlock Holmes', price: 18.49)
 
-30.times do
+10.times do
   book.sells << Sell.create(quantity: rand(2..15), day: Sell::DAYS[rand(0..(Sell::DAYS.length))])
+end
+
+10.times do
+  book2.sells << Sell.create(quantity: rand(2..15), day: Sell::DAYS[rand(0..(Sell::DAYS.length))])
+end
+
+10.times do
+  book3.sells << Sell.create(quantity: rand(2..15), day: Sell::DAYS[rand(0..(Sell::DAYS.length))])
 end
