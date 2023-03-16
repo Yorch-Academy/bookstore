9bf7b3616d1706b859722b98919b966cb33c33e0
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-16 16:40:42 -0600

    Revenuw helper.

--- app/helpers/sells_helper.rb
+++ app/helpers/sells_helper.rb
@@ -8,4 +8,14 @@ module SellsHelper
 
     array
   end
+
+  def revenue_from_sells
+    array = Array.new
+
+    Book.all.each do |book|
+      array.push([book.name, book.sells.sum(:total).round(2)])
+    end
+
+    array
+  end
 end
